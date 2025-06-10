287ff4005ff359945d813ca9866c0e1cedc138c1
xhihatun <hihatun@havelsan.com.tr>
2025-06-10 15:17:29 +0300

    main py udpate

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api import router
 import uvicorn
+import os 
+
+os.environ['HUGGINGFACE_TOKEN'] = os.getenv("HUGGINGFACE_TOKEN")
+os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
+os.environ["LANGSMITH_API_KEY"] = os.getenv("LANGSMITH_API_KEY")
 
 app = FastAPI(title="English Accent Analyzer")
 
@@ -21,6 +30,6 @@ app.include_router(router)
 def read_root():
     return {"message": "Welcome to the Accent Analyzer API."}
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
